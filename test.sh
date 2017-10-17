8e56819cbb83b9d1ae3e15fce52241055e416081
dddm49 <dddm49@Hotmail.com>
2017-10-17 10:15:56 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,14 +3,5 @@ echo "This is test.sh "
 echo "This is new repository 2"
 
 
-sudo apt-get install openjdk-8-jdk
-sudo apt-get install openjdk-8-jre
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add - 
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' 
-sudo apt-get update 
-sudo apt-get install jenkins
-
-sudo apt-get update 
-sudo apt-get install jenkins
 
 
