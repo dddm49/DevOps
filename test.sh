7ab0ad716e909dfb4aaa3f0b1493129e0b9a8c72
dddm49 <dddm49@Hotmail.com>
2017-11-08 14:49:58 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ echo "Installing Jenkins CI Server"
 
 add-apt-repository ppa:webupd8team/java
 apt-get update
-apt-get install oracle-java8-installer
+apt-get -y install oracle-java8-installer
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
