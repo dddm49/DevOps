55426754224508a6cf572c7f953282c4447cce13
dddm49 <dddm49@Hotmail.com>
2017-11-16 11:44:06 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,10 +4,11 @@
 
 # Jenkins
 echo "Installing Jenkins CI Server"
-
-add-apt-repository ppa:webupd8team/java
-apt-get update
-apt-get -y install oracle-java8-installer
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
